103d2f14f6659455a54a114a0a1aaae25c8c0558
Elton Lopes Pereira <87839129+Elton-Ti@users.noreply.github.com>
2023-10-02 19:36:19 -0300

    ¨modal¨

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2023</li>                
+                <li>&copy; 2023</li>                
               </ul>
             </div>
           </Fade>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,20 +1,49 @@
 import React, { Component } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
+import Modal from "react-modal"; // Importe o react-modal
 
 let id = 0;
+
 class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      selectedProject: null,
+    };
+  }
+
+  openModal = (project) => {
+    this.setState({
+      showModal: true,
+      selectedProject: project,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+      selectedProject: null,
+    });
+  };
+
   render() {
     if (!this.props.data) return null;
 
-    const projects = this.props.data.projects.map(function (projects) {
-      let projectImage = "images/portfolio/" + projects.image;
+    const projects = this.props.data.projects.map((project) => {
+      let projectImage = "images/portfolio/" + project.image;
 
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <Zmage
+              alt={project.title}
+              src={projectImage}
+              onClick={() => this.openModal(project)} // Abra o modal ao clicar na imagem
+            />
+            <div style={{ textAlign: "center" }}>{project.title}</div>
           </div>
         </div>
       );
@@ -36,6 +65,56 @@ class Portfolio extends Component {
             </div>
           </div>
         </Fade>
+
+        {/* Modal */}
+        <Modal
+          isOpen={this.state.showModal}
+          onRequestClose={this.closeModal}
+          contentLabel="Project Description"
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.75)",
+            },
+            content: {
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "80%",
+              maxWidth: "700px",
+              border: "none",
+              borderRadius: "8px",
+              padding: "20px",
+            },
+          }}
+        >
+          <button
+            onClick={this.closeModal}
+            style={{
+              position: "absolute",
+              top: "10px",
+              right: "10px",
+              backgroundColor: "transparent",
+              border: "none",
+              cursor: "pointer",
+              fontSize: "24px",
+              color: "red",
+            }}
+          >
+            &times;
+          </button>
+          {this.state.selectedProject && (
+            <div>
+              <h2>{this.state.selectedProject.title}</h2>
+              <p>{this.state.selectedProject.description}</p> 
+              <h6>{this.state.selectedProject.alunos}</h6>  
+              <span>{this.state.selectedProject.text}</span>
+              <p></p>     
+              <a href={this.state.selectedProject.url} target="_blank" rel="noopener noreferrer">
+                Saiba mais
+              </a>     
+            </div>
+          )}
+        </Modal>
       </section>
     );
   }
